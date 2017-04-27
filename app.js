45864301fc53ad648b4f7c5e63fb4f2aaef4a721
Ryan O'Dell <ryan.odell79@gmail.com>
2017-04-27 16:12:26 -0600

    Add a Guitar list

--- app.js
+++ app.js
@@ -1,78 +1,36 @@
-var bookSeed = [
+var guitars = [
   {
-    book_id: "1",
-    title: "One Piece",
-    cover: "https://myanimelist.cdn-dena.com/images/anime/6/73245.jpg",
-    description: "One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha'\s Weekly Shōnen Jump magazine since July 19, 1997, with the chapters collected into eighty-four tankōbon volumes to date.",
-    author: "Eiichiro Oda",
-    publish: 1997,
-     },
+    guitar_id: "1",
+    name: "Jeff Beck Strat",
+    image: "https://s-media-cache-ak0.pinimg.com/originals/5b/aa/71/5baa715df4fd396bac2bd44f4d5f98a9.jpg",
+    description: "Surf Green with Lace Sensors",
+    manufacture: "Fender",
+    year: 1996,
+  },
   {
-    book_id: "2",
-    title: "One Punch Man",
-    cover: "https://myanimelist.cdn-dena.com/images/anime/12/76049.jpg",
-    description: "One-Punch Man is an ongoing Japanese superhero parody webcomic created by an author using the pseudonym One which began publication in early 2009. The series quickly went viral, surpassing 7.9 million hits in June 2012.",
-    author: "One",
-    publish: 2012,
-     },
-  {
-    book_id: "3",
-    title: "Sword Art Online",
-    cover: "https://myanimelist.cdn-dena.com/images/anime/11/39717.jpg",
-    description: "Sword Art Online is a 2009 Japanese light novel series written by Reki Kawahara and illustrated by Abec. The series takes place in the near future and focuses on various virtual reality MMORPG worlds.",
-    author: "Reki Kawahara",
-    publish: 2009,
-     },
-  {
-    book_id: "4",
-    title: "Tokyo Ghoul",
-    cover: "https://myanimelist.cdn-dena.com/images/anime/5/64449.jpg",
-    description: "Tokyo Ghoul is a Japanese dark fantasy manga series by Sui Ishida.",
-    author: "Sui Ishida",
-    publish: 2011,
-     },
-  {
-    book_id: "5",
-    title: "Hai to Gensou no Grimgar",
-    cover: "https://myanimelist.cdn-dena.com/images/anime/13/77976.jpg",
-    description: "Grimgar of Fantasy and Ash is a Japanese light novel series written by Ao Jūmonji and illustrated by Eiri Shirai.",
-    author: "Ao Jumonji",
-    publish: 2016,
+    guitar_id: "2",
+    name: "Jimi Hendrix Gibson Flying V",
+    image: "http://68.media.tumblr.com/b7a2f15fa3f1e6390d907346168db7a0/tumblr_nmt8dbagE81texqglo2_1280.jpg",
+    description: "Black with 490R and 490T Pickups",
+    manufacture: "Gibson",
+    year: 1991,
      }
 ]
 
-var titles = [];
+var names = [];
 
 $(document).ready(function(){
-  if (!localStorage.getItem('books')) {
-    localStorage.setItem('books', JSON.stringify(bookSeed));
+  for (var i = 0; i < guitars.length; i++) {
+      names.push('<li class=' + (JSON.stringify(guitars[i].guitar_id)) + '>' + (JSON.stringify(guitars[i].name)) + '</li>');
   }
-  var books = JSON.parse(localStorage.getItem('books'));
-  for (var i = 0; i < books.length; i++) {
-      titles.push('<li class=' + (JSON.stringify(books[i].book_id)) + '>' + (JSON.stringify(books[i].title)) + '</li>');
-  }
-  $('#list').html(titles.join(''));
+  $('#list').html(names.join(''));
   $(document).on('click', 'li', function(e){
     $('h2').remove();
-    var currentBook = books[e.currentTarget.className -1];
-    $('#poster').attr('src', currentBook.cover);
-    $('#title').html('Title: ' + currentBook.title);
-    $('#author').html('Author: ' + currentBook.author);
-    $('#publish').html('Published: ' + currentBook.publish);
-    $('#description').html('Description: ' + currentBook.description);
-  })
-  $('#submitter').on('click', function(e){
-    e.preventDefault();
-    var newestBook = {
-      book_id: newBook.book_id.value,
-      title: newBook.title.value,
-      cover: newBook.cover.value,
-      description: newBook.description.value,
-      author: newBook.author.value,
-      publish: newBook.publish.value,
-       };
-    books.push(newestBook);
-    localStorage.setItem('books', JSON.stringify(books));
-    window.location.reload();
+    var currentGuitar = guitars[e.currentTarget.className -1];
+    $('#image').attr('src', currentGuitar.image);
+    $('#name').html('Name: ' + currentGuitar.name);
+    $('#description').html('Description: ' + currentGuitar.description);
+    $('#manufacture').html('Manufacture: ' + currentGuitar.manufacture);
+    $('#year').html('Year: ' + currentGuitar.year);
+    })
   });
-});
